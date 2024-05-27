8a1b36a1bd5f7bb9709acfa1a52548c9737f4670
lenoel777 <glenn.leonali@binus.ac.id>
2024-05-27 08:46:11 +0700

    Initial Commit

---
+++ ReleaseApp/ReleaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReleaseAppApp.swift
+//  ReleaseApp
+//
+//  Created by Glenn Leonali on 27/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ReleaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
